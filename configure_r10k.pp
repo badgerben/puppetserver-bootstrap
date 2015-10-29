3edbbf1ebca5337d4a58abade61dc7f0a960444e
Ben Huebner <ben@huebner.us>
2015-10-28 19:20:09 -0500

    need to create r10k config directory

--- configure_r10k.pp
+++ configure_r10k.pp
@@ -6,7 +6,18 @@ sshkey { $::fqdn:
   type   => 'ssh-rsa',
 }
 
+# create directory for r10k config file
+file { '/etc/puppetlabs/r10k':
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
 # install r10k
 class { 'r10k':
   remote => "gitolite3@${::fqdn}:puppet-control.git",
 }
+
+# r10k class depends on r10k config directory
+File['/etc/puppetlabs/r10k'] -> Class['r10k']
