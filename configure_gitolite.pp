6c023c72119b8f8dc7c0577fa5dcb36d856e2f51
Ben Huebner <ben@huebner.us>
2015-10-30 14:09:47 -0500

    allow local code in gitolite

--- configure_gitolite.pp
+++ configure_gitolite.pp
@@ -5,6 +5,8 @@ include epel
 class { 'gitolite':
   admin_key_source    => 'file:///root/.ssh/id_rsa.pub',
   git_config_keys     => '.*',
+  allow_local_code    => true,
+  local_code_in_repo  => true,
   repo_specific_hooks => true,
 }
 
