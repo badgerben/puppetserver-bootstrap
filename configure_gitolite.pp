521423b75b7c1eb95917394b56ddfabcbf253f86
Ben Huebner <ben@huebner.us>
2015-11-11 06:30:34 -0600

    set umask param for gitolite

--- configure_gitolite.pp
+++ configure_gitolite.pp
@@ -5,6 +5,7 @@ include epel
 class { 'gitolite':
   admin_key_source    => 'file:///root/.ssh/id_rsa.pub',
   git_config_keys     => '.*',
+  umask               => '0077',
   allow_local_code    => true,
   local_code_in_repo  => true,
   repo_specific_hooks => true,
