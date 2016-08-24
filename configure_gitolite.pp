650fc7aea725f2b692e9635cb6c959929692d383
Ben Huebner <ben@huebner.us>
2016-08-24 16:09:01 -0500

    update configure_gitolite.pp manifest
    removed umask param (fixed in new version of module). change epel
    declaration from include to class. move require metaparam to gitolite
    class definition.

--- configure_gitolite.pp
+++ configure_gitolite.pp
@@ -1,15 +1,12 @@
 # install epel (stahnma-epel module)
-include epel
+class { 'epel': }
 
 # install gitolite (echoes-gitolite module)
 class { 'gitolite':
   admin_key_source    => 'file:///root/.ssh/id_rsa.pub',
   git_config_keys     => '.*',
-  umask               => '0077',
   allow_local_code    => true,
   local_code_in_repo  => true,
   repo_specific_hooks => true,
+  require             => Class['epel'],
 }
-
-# gitolite depends on epel
-Class['epel'] -> Class['gitolite']
