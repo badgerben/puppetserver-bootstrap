9989273faad2c51fdad5ca32218ed139d030fdb6
Ben Huebner <ben@huebner.us>
2016-08-24 16:13:18 -0500

    Update configure_r10k.pp manifest
    Removed resource to create r10k directory (fixed in new version of
    module). removed dependency to file resource.

--- configure_r10k.pp
+++ configure_r10k.pp
@@ -7,19 +7,8 @@ sshkey { $::fqdn:
   type         => 'ssh-rsa',
 }
 
-# create directory for r10k config file
-file { '/etc/puppetlabs/r10k':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0755',
-}
-
 # install r10k
 class { 'r10k':
   remote   => "gitolite3@${::fqdn}:puppet-control.git",
   cachedir => '/opt/puppetlabs/puppet/cache/r10k',
 }
-
-# r10k class depends on r10k config directory
-File['/etc/puppetlabs/r10k'] -> Class['r10k']
